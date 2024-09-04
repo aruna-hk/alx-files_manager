b7c1c4cc8981953ea267ad7a1b9c7915b8088ea4
hk <kiptooharon.hk@gmail.com>
2024-09-05 02:08:44 +0300

    api routes

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+import { AppController } from '../controllers/AppController';
+
+const express = require('express');
+
+const routes = express.Router();
+routes.get('/status', AppController.getStatus);
+routes.get('/stats', AppController.getStats);
+
+export {
+  routes
+}
